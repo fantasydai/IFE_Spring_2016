1accdaa96716dedc532d99834ee1e128faf88462
daiyueyue <976784952@qq.com>
2016-04-29 22:58:40 +0800

    commit for task3/40

--- task3/task40/main.js
+++ task3/task40/main.js
@@ -1,4 +1,8 @@
-var init;
+var init,
+      data={
+        startDate:"1990/1/1",
+        endDate:"2016/9/30"
+      };
 (function(){
     var initialize=function(){//初始化渲染页面
         var today=new Date();
@@ -7,9 +11,9 @@ var init;
     var dayArray=function(year,month){//创建存放日期的函数
         var firstDay=new Date(year,month-1,1).getDay(),//求出当月第一天是星期几
         days=new Date(year,month,0).getDate(),//求出当月天数，下个月第零天就是当月最后一天
-        arr=new Array(35);//创建数组，存放日期
+        arr=new Array(42);//创建数组，存放日期
         for(var i=0;i<days;i++,firstDay++){
-            arr[firstDay]=year+"-"+month+"-"+(i+1);
+            arr[firstDay]=year+"/"+month+"/"+(i+1);
         }
         return arr;
     };
@@ -57,14 +61,18 @@ var init;
                     weekItem.className="week";
                     fragment.appendChild(weekItem);
              }
-            //创建星期显示区
+            //创建日期显示区
             for(var k=0;k<dayArr.length;k++){
                            var dayItem=a.cloneNode(true);
                             if(dayArr[k]==undefined){
                             fragment.appendChild(dayItem);
                             }else{
-                                    dayItem.innerHTML=dayArr[k].split("-")[2];
+                                    dayItem.innerHTML=dayArr[k].split("/")[2];
                                     dayItem.className="day";
+                                    var newDay=Date.parse(new Date(dayArr[k]));
+                                    if(newDay<Date.parse(new Date(data.startDate))||newDay>Date.parse(new Date(data.endDate))){
+                                        dayItem.className+=" exceed";
+                                    }
                                     if(k%7===0||k%7===6){
                                             dayItem.className+=" weekend";
                                     }
@@ -72,6 +80,22 @@ var init;
                             }
             }
             calendar.appendChild(fragment);
+                //显示选中日期的函数
+            var dateShow=function (day){
+                        var showInput=document.getElementById("showDate"),
+                            yearAndMonth=document.getElementsByClassName("btn")[1].nextSibling;
+                        showInput.value=yearAndMonth.data+day+"日";
+             };
+                    //点击日期时间处理程序
+            var dateClick = function (){
+                     var target=event.target,
+                            day=target.innerHTML;
+                     if(target.className.indexOf("day")>-1&&target.className.indexOf("exceed")==-1){
+                             dateShow(day);
+                     }
+                };
+                //为日历元素添加点击事件处理程序
+                calendar.addEventListener("click",dateClick,false);
     };
 
     //点击按钮重新绘制日历内容
@@ -100,7 +124,6 @@ var init;
         }
             drawCalendar(year,month,date);
         };
-
        init=initialize;
 })();
 init();
